b6833f323f9b6f83760aa8c98d08b8903ecb0eab
Saifur Rehman Khan <saifurrahmankhankhan@gmail.com>
2021-02-09 12:05:02 +0530

    fixed bugs

--- src/animation/AnimateLogos.tsx
+++ src/animation/AnimateLogos.tsx
@@ -1,31 +1,8 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import javascript from '../assests/javascript.png';
-import $ from 'jquery';
 
 const AnimateLogos = () => {
 
-    // const makeNewPosition = () => {
-    //     const h = $(window).height() - 50;
-    //     const w = $(window).width() - 50;
-        
-    //     const nh = Math.floor(Math.random() * h);
-    //     const nw = Math.floor(Math.random() * w);
-        
-    //     return [nh,nw]; 
-    // }
-
-    const animateDiv = ( myclass : string ) => {
-        // const newq = makeNewPosition();
-        const newq = [1,2];
-
-        $(myclass).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
-            animateDiv(myclass);        
-        });
-    }
-
-    useEffect(() => {
-        animateDiv("javascript");
-    },[]);
 
     return (
         <div className="javascript">
