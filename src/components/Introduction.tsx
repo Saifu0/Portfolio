52067b62c32a47432ca8b3f3e9361147a8758aa1
Saifu0 <saifurrahmankhankhan@gmail.com>
2021-05-31 08:42:23 +0530

    added resume

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -9,10 +9,13 @@ function Introduction() {
                 <img className="photo" src={Me} alt="profile"/>
             </div>
             <div className="intro" style={{marginLeft : 20, color : "black"}} >
-                Hi, I'm Saifur Rehman. I'm a Computer Science Stduent, Software Developer and a Competitive Programmer(atleast I claim). I like to build full-stack applications,
-                solving complex Software engineering, Data Structure and Algorithm based problems and desiging highly scable and highly available systems.
-                I have good knowledge of Computer Science fundamentals including System Design, Computer Networks, Databases, Operating Systems and Object-Oriented Design.
-                I love to compete in coding competitons and watch anime in my spare time.
+                I'm a Computer Science Student, Software Developer, and Competitive Programmer. 
+                I like to build full-stack applications, solving complex Software engineering, 
+                Data Structure, and Algorithm based problems, and designing highly scalable and 
+                highly available systems. I have good knowledge of Computer Science fundamentals 
+                including System Design, Computer Networks, Databases, Operating Systems, and 
+                Object-Oriented Design. I love to compete in coding competitions and watch anime 
+                in my spare time.
             </div>
         </div>
     )
